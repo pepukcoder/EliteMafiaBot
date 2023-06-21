068cdcb1d14c74b902ea258fef14624a70550760
Um1aut <pavel.manokhin228@gmail.com>
2023-06-21 19:03:30 +0200

    added game states

--- src/handlers/messages/chat/start/start.py
+++ src/handlers/messages/chat/start/start.py
@@ -1,17 +1,23 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 join_button = InlineKeyboardButton(text='Присоединиться', callback_data="join_handler")
-
 inline = InlineKeyboardMarkup().add(join_button)
+
+connected_users = set()
 
 def register_start_handlers(dp: Dispatcher):
     @dp.message_handler(commands=['game'])
     async def start_game(message: types.Message):
-        await message.reply("Набор в игру начан! \n Присоединившиеся игроки:", reply_markup=inline)
-    @dp.callback_query_handler(text=["join_handler"])
-    async def game_join(call: types.CallbackQuery):
-        if call.data == 'join_handler':
-            await call.message.answer('@Umlaut')
+        msg = await message.reply("Набор в игру начат!", reply_markup=inline)
+        @dp.callback_query_handler(text=["join_handler"])
+        async def game_join(call: types.CallbackQuery):
+                if call.data == 'join_handler':
+                    connected_users.add(call.from_user['first_name'])
+                    try:
+                        await msg.edit_text("Набор в игру начат!\n*Всего игроков: " + str(len(connected_users)) + '*\n' + ', '.join(connected_users), reply_markup=inline, parse_mode= 'Markdown')
+                    except:
+                        await call.answer("Да ты уже в игре, долбоёб!")
+                    await call.answer()
 
     pass
---
+++ src/state/__init__.py
@@ -0,0 +1,5 @@
+from .game_state import GameState
+from .state import State
+from .user_state import UserState
+from .vote_state import VoteState
+from .interaction_history_record import InteractionHistoryRecord
