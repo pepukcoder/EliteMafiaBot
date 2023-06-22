906da99ef298540a06ab5cdc51bc935df5f9b0a7
Um1aut <pavel.manokhin228@gmail.com>
2023-06-22 17:43:00 +0200

    added gamejoin through a bot

--- src/handlers/messages/chat/start/start.py
+++ src/handlers/messages/chat/start/start.py
@@ -1,9 +1,8 @@
 from aiogram import Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from src.state import State, RegistrationState, UserInfo
+from aiogram.utils.deep_linking import get_start_link
 
-join_button = InlineKeyboardButton(text='Присоединиться', callback_data="join_handler")
-inline = InlineKeyboardMarkup().add(join_button)
 
 
 def register_start_handlers(dp: Dispatcher):
@@ -14,36 +13,14 @@ def register_start_handlers(dp: Dispatcher):
         chat_id = message.chat.id
         state.registrations.append(RegistrationState(chat_id, {}))
 
-        msg = await message.reply("Набор в игру начат!", reply_markup=inline)
+        msg = await message.reply("Набор в игру начат!")
+        link = await get_start_link(str(chat_id) + ", " + str(msg['message_id']), encode=True)
+        join_button = InlineKeyboardButton(text='Присоединиться', url=link)
+        inline = InlineKeyboardMarkup().add(join_button)
+        
+        await msg.edit_text("Набор в игру начат!", reply_markup=inline)
 
-    @dp.callback_query_handler(text=["join_handler"])
-    async def game_join(call: types.CallbackQuery):
-        if call.data == 'join_handler':
-            message = call.message
-            chat_id = call.message.chat.id
-            user_id = call.from_user.id
-            username = call.from_user.username
-            first_name = call.from_user.first_name
-            registration_state = list(filter(lambda x: x.chat_id == chat_id, state.registrations))[0]
-            if not registration_state:
-                await call.answer("Problem")
-
-            registration_state.users[user_id] = UserInfo(first_name, username)
-
-            first_names = [item.first_name for item in registration_state.users.values()]
-            usernames = [item.username for item in registration_state.users.values()]
-
-            formatted_list = [f"[{name}](t.me/{username})" for name, username in zip(first_names, usernames)]
-            print(registration_state)
-            try:
-                await message.edit_text(
-                    "Набор в игру начат!\n*Всего игроков: " + str(len(registration_state.users.keys())) + '*\n' +
-                    ', '.join(formatted_list), reply_markup=inline, parse_mode='Markdown',
-                    disable_web_page_preview=True)
-            except:
-                await call.answer("Да ты уже в игре, долбоёб!")
-
-    @dp.message_handler(commands=['start'])
+    @dp.message_handler(commands=['start_game'])
     async def send_welcome(message: types.Message):
         registration_state = list(filter(lambda x: x.chat_id == message.chat.id, state.registrations))[0]
         print(registration_state)
--- src/handlers/messages/user/main.py
+++ src/handlers/messages/user/main.py
@@ -1,6 +1,48 @@
-from aiogram import Dispatcher
-
+from aiogram import Dispatcher, Bot
+from aiogram.utils.deep_linking import decode_payload
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from src.state import State, RegistrationState, UserInfo
+from aiogram.utils.deep_linking import get_start_link
 
 def register_user_handlers(dp: Dispatcher):
-    # todo: register user handlers
+    bot = Bot(token="6170395643:AAHiJFyprovmr6AgyUo4wdSIWQQQlrvMpJY", parse_mode='HTML')
+    state = State()
+
+    @dp.message_handler(commands=["start"])
+    async def handler(message: Message):
+        args = message.get_args()
+        payload = decode_payload(args)
+        print(args)
+        join_button = InlineKeyboardButton(text='Присоединиться', url="https://t.me/elite_mafia_bot?start="+args)
+        inline = InlineKeyboardMarkup().add(join_button)
+
+        if payload:
+            chat_id = int(payload.split(',', 1)[0])
+            message_id = int(payload.split(',', 1)[-1].replace(" ", ""))
+            user_id = message.from_user.id
+            username = message.from_user.username
+            first_name = message.from_user.first_name
+            print(message_id, chat_id, user_id, username, first_name)
+
+            registration_state = list(filter(lambda x: x.chat_id == chat_id, state.registrations))[0]
+            if not registration_state:
+                await message.answer("Problem")
+
+            registration_state.users[user_id] = UserInfo(first_name, username)
+
+            first_names = [item.first_name for item in registration_state.users.values()]
+            usernames = [item.username for item in registration_state.users.values()]
+
+            formatted_list = [f"[{name}](t.me/{username})" for name, username in zip(first_names, usernames)]
+            print(registration_state)
+            try:
+                await bot.edit_message_text(chat_id=chat_id, message_id=message_id,
+                    text=("Набор в игру начат!\n*Всего игроков: " + str(len(registration_state.users.keys())) + '*\n' +
+                    ', '.join(formatted_list)), parse_mode='Markdown', reply_markup=inline,
+                    disable_web_page_preview=True)
+                await message.answer(f"Ты зарегестрировался в игре.")
+            except:
+                await message.answer('ДА ТЫ УЖЕ В ИГРЕ, ДОЛБОЁБ!')
+        else:
+            await message.answer(f"Yчё")
     pass
