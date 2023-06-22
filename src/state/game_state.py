28c8c69ce671f2fb298e4d99ff2955a16456b57f
illia hrushchenko <gr.illya13@gmail.com>
2023-06-22 23:20:56 +0300

    Deleted unused property is_started in game_state.py

--- src/state/game_state.py
+++ src/state/game_state.py
@@ -9,7 +9,6 @@ from .interaction_history_record import InteractionHistoryRecord
 class GameState:
     day: int
     chat_id: int
-    is_started: bool
     is_day: bool
     users: list[UserState]
     votes: list[VoteState]
