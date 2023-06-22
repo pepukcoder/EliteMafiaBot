4b130db62456d65a7f71e263a278ea425600aec3
illia hrushchenko <gr.illya13@gmail.com>
2023-06-22 22:03:49 +0300

    Added abstract role to state

---
+++ src/state/abstractions/role.py
@@ -0,0 +1,13 @@
+from abc import abstractmethod, ABC
+
+from aiogram import Bot
+
+
+class Role(ABC):
+    @abstractmethod
+    def get_role_name(self):
+        pass
+
+    @abstractmethod
+    def send_interactive_messages(self, bot: Bot):
+        pass
