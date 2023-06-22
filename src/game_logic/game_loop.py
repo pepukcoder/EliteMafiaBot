f03613a4e68f0dda28261faf64ede4dc30784f5b
illia hrushchenko <gr.illya13@gmail.com>
2023-06-22 23:32:39 +0300

    Added interaction_types and edited game_loop.py

--- src/game_logic/game_loop.py
+++ src/game_logic/game_loop.py
@@ -1,11 +1,14 @@
-def start_day():
+async def start_day():
     # assign_roles
     # night_roles_act
+    # wait_until_all_users_interact
+    # set_day
     # show_int_history
     # show_alive
     # win_check
     # vote
     # check_votes
     # vote_kill
+    # set_night
     # increment_day
     pass
--- src/state/enums/interaction_types.py
+++ src/state/enums/interaction_types.py
@@ -2,4 +2,7 @@ import enum
 
 
 class InteractionTypes(enum.IntEnum):
-    kill = 0
+    nothing = 0
+    kill = 1
+    heal = 2
+    check = 3
