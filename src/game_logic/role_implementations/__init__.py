47b19f1fe024b3d54851a8890d16c7eb614f9b9f
illia hrushchenko <gr.illya13@gmail.com>
2023-06-22 22:06:14 +0300

    Added implementation classes to game logic without implementation

---
+++ src/game_logic/role_implementations/__init__.py
@@ -0,0 +1,4 @@
+from .don import Don
+from .townie import Townie
+from .detective import Detective
+from .doctor import Doctor
---
+++ src/game_logic/role_implementations/townie.py
@@ -0,0 +1,11 @@
+from aiogram import Bot
+
+from src.state import Role
+
+
+class Townie(Role):
+    def get_role_name(self):
+        raise NotImplementedError()
+
+    def send_interactive_messages(self, bot: Bot):
+        pass
