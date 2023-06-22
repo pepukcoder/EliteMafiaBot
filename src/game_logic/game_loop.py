849bebc9ad870193096ed89b8218669955066d95
illia hrushchenko <gr.illya13@gmail.com>
2023-06-22 23:26:48 +0300

    Added game_loop.py

---
+++ src/game_logic/game_loop.py
@@ -0,0 +1,11 @@
+def start_day():
+    # assign_roles
+    # night_roles_act
+    # show_int_history
+    # show_alive
+    # win_check
+    # vote
+    # check_votes
+    # vote_kill
+    # increment_day
+    pass
