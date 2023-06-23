7d1445785bff2e3f0342732eea012e9c1e078c14
illia hrushchenko <gr.illya13@gmail.com>
2023-06-23 19:04:04 +0300

    Implemented sending strategies

---
+++ src/game_logic/sending_strategies/send_role_name_messages_strategy.py
@@ -0,0 +1,12 @@
+from aiogram import Bot
+
+from src.game_logic.sending_strategies import Strategy
+from src.state import State
+
+
+class SendRoleNameMessagesStrategy(Strategy):
+    async def send(self, game_chat_id: int, bot: Bot):
+        state = State()
+        game = list(filter(lambda x: x.chat_id == game_chat_id, state.games))[0]
+        for user in game.users:
+            user.role.send_role_name(bot)
