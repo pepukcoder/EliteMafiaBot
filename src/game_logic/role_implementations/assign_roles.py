8f2c47f33a12babed2caa7a5ce941013b8b21724
Um1aut <pavel.manokhin228@gmail.com>
2023-06-23 12:21:14 +0200

    edited role_implementations

--- src/game_logic/role_implementations/assign_roles.py
+++ src/game_logic/role_implementations/assign_roles.py
@@ -1,46 +1,97 @@
-from aiogram import Bot
-
 import random
 import enum
+from abc import abstractmethod, ABC
+#from src.state import UserState
+class Role(ABC):
+    @abstractmethod
+    def get_role_name(self):
+        pass
+
+class Don(Role):
+    def get_role_name(self):
+        raise NotImplementedError()
+
+
+from dataclasses import dataclass
+@dataclass
+class UserState:
+    username: str
+    user_id: int
+    role: Role
 
 class Roles(enum.Enum):
-    DON = "Дон"
-    MAFIA = "Мафия"
-    PIZDABOL = "Пиздабол"
-    OSVEDOMITEL = "Осведомитель"
-    DOCTOR = "Доктор"
-    COMMISSAR = "Коммисар"
-    SHLYUKHA = "Шлюха"
-    OMEZHKA = "Омежка"
-    SUICIDNIK = "Суицидник"
-    ADVOKAT = "Адвокат"
-    ALFACH = "Альфач"
+    DON = 1
+    MAFIA = 2
+    LIAR = 3
+    INFORMANT = 4
+    DOCTOR = 5
+    DETECTIVE = 6
+    WHORE = 7
+    OMEGA = 8
+    LAWYER = 9
+    ALFA = 10
 
-roles_count = {
-    4: {Roles.DON: 1, Roles.DOCTOR: 1, Roles.MAFIA: 0, Roles.COMMISSAR: 0, Roles.PIZDABOL: 0,
-        Roles.OSVEDOMITEL: 0, Roles.SHLYUKHA: 0, Roles.OMEZHKA: 0, Roles.SUICIDNIK: 0,
-        Roles.ADVOKAT: 0, Roles.ALFACH: 0},
-    5: {Roles.DON: 1, Roles.DOCTOR: 1, Roles.MAFIA: 1, Roles.COMMISSAR: 0, Roles.PIZDABOL: 0,
-        Roles.OSVEDOMITEL: 0, Roles.SHLYUKHA: 0, Roles.OMEZHKA: 0, Roles.SUICIDNIK: 0,
-        Roles.ADVOKAT: 0, Roles.ALFACH: 0},
-    6: {Roles.DON: 1, Roles.DOCTOR: 1, Roles.MAFIA: 0, Roles.COMMISSAR: 1, Roles.PIZDABOL: 0,
-        Roles.OSVEDOMITEL: 0, Roles.SHLYUKHA: 0, Roles.OMEZHKA: 0, Roles.SUICIDNIK: 0,
-        Roles.ADVOKAT: 0, Roles.ALFACH: 1},
-    7: {Roles.DON: 1, Roles.DOCTOR: 1, Roles.MAFIA: 1, Roles.COMMISSAR: 1, Roles.PIZDABOL: 0,
-        Roles.OSVEDOMITEL: 0, Roles.SHLYUKHA: 0, Roles.OMEZHKA: 0, Roles.SUICIDNIK: 0,
-        Roles.ADVOKAT: 0, Roles.ALFACH: 1},
-    8: {Roles.DON: 1, Roles.DOCTOR: 1, Roles.MAFIA: 1, Roles.COMMISSAR: 1, Roles.PIZDABOL: 0,
-        Roles.OSVEDOMITEL: 0, Roles.SHLYUKHA: 0, Roles.OMEZHKA: 0, Roles.SUICIDNIK: 0,
-        Roles.ADVOKAT: 0, Roles.ALFACH: 1}
-}
+BAD_ROLES = [Roles.DON, Roles.MAFIA, Roles.LIAR, Roles.INFORMANT]
+ADDITIONAL_ROLES = [Roles.WHORE, Roles.OMEGA, Roles.LAWYER, Roles.ALFA]
+GOOD_ROLES = [Roles.DETECTIVE, Roles.DOCTOR]
 
-def assign_roles_randomly(player_count):
-    roles = [role for role, count in roles_count[player_count].items() for _ in range(count)]
-    random.shuffle(roles)
-    return roles[:player_count]
+def insert_none_position(lst, value):
+    none_indices = [i for i, elem in enumerate(lst) if elem is None]
+    if none_indices:
+        random_index = random.choice(none_indices)
+        lst[random_index] = value
 
-player_count = 5
+def assign_roles(player_count):
+    if player_count == 4:
+        return assign_roles_scenario_4()
+    elif player_count == 5:
+        return assign_roles_scenario_5()
+    elif player_count == 6:
+        return assign_roles_scenario_6()
+    else:
+        raise ValueError("Invalid number of players.")
 
-assigned_roles = assign_roles_randomly(player_count)
-for i, role in enumerate(assigned_roles):
-    print(f"Player {i+1}: {role.value}")
+def assign_roles_scenario_4():
+    player_roles = [None] * 4
+    unique_indices = random.sample(range(4), 2)
+    player_roles[unique_indices[0]] = Roles.DON
+    player_roles[unique_indices[1]] = Roles.DOCTOR
+    return player_roles
+
+def assign_roles_scenario_5():
+    player_roles = [None] * 5
+    unique_indices = random.sample(range(5), 3)
+    player_roles[unique_indices[0]] = Roles.DON
+    player_roles[unique_indices[1]] = Roles.DOCTOR
+    x = random.sample(ADDITIONAL_ROLES, 1)
+    player_roles[unique_indices[2]] = x[0]
+    return player_roles
+
+
+def assign_roles_scenario_6():
+    player_roles = assign_roles_scenario_4()
+
+    bad_role = random.choice([role for role in BAD_ROLES if role != Roles.MAFIA and role != Roles.DON])
+    player_roles.append(bad_role)
+    player_roles.append(Roles.DETECTIVE)
+    if random.random() < 0.5:
+        insert_none_position(player_roles, random.sample(ADDITIONAL_ROLES, 1)[0])
+    else:
+        pass
+    return player_roles
+
+# Example usage
+player_count = 6
+player_roles = assign_roles(player_count)
+
+users = [
+    UserState(username="Alice", user_id=1, role=player_roles[0]),
+    UserState(username="Bob", user_id=2, role=player_roles[1]),
+    UserState(username="Charlie", user_id=3, role=player_roles[2]),
+    UserState(username="Dave", user_id=4, role=player_roles[3]),
+    UserState(username="Pasha", user_id=5, role=player_roles[4]),
+    UserState(username="Pasha2", user_id=6, role=player_roles[5])
+]
+
+# Print the assigned roles
+print(users)
--- src/game_logic/role_implementations/liar.py
+++ src/game_logic/role_implementations/liar.py
@@ -3,9 +3,9 @@ from aiogram import Bot
 from src.state import Role
 
 
-class Townie(Role):
+class Liar(Role):
     def get_role_name(self):
         raise NotImplementedError()
 
     async def send_interactive_messages(self, bot: Bot):
-        pass
+        raise NotImplementedError()
