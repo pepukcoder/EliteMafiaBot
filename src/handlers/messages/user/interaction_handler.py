27c354c1eec54e044a8bc0ef411a43e8949aeccc
illia hrushchenko <gr.illya13@gmail.com>
2023-06-26 23:13:44 +0300

    Fixed merge

--- src/handlers/messages/user/interaction_handler.py
+++ src/handlers/messages/user/interaction_handler.py
@@ -23,7 +23,7 @@ def register_interaction_handler(dp: Dispatcher):
         print(state.games)
 
         await call.message.answer("Вы выбрали цель")
-        await bot.send_message(chat_id= int(chat_id),text=f"{str(get_role_by_user_id(chat_id=int(chat_id), user_id=int(user_id)))} {get_role_by_user_id(chat_id=int(chat_id), user_id=int(user_id)).interaction_message()}")
+        await bot.send_message(chat_id= int(chat_id),text=f"{str(get_role_by_user_id(chat_id=int(chat_id), user_id=int(user_id)))} {get_role_by_user_id(chat_id=int(chat_id), user_id=int(user_id)).get_interaction_message()}")
         await call.message.delete()
 
 
