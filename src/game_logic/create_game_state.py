2442116dc587ef913aa9044e73321a2f38e74474
illia hrushchenko <gr.illya13@gmail.com>
2023-06-26 22:00:17 +0300

    Added explicit params

--- src/game_logic/create_game_state.py
+++ src/game_logic/create_game_state.py
@@ -7,6 +7,6 @@ def create_game_state(chat_id: int):
     try:
         game = state.games[chat_id]
     except KeyError:
-        state.games[chat_id] = GameState(0, False, users=[], votes=[], interaction_history=[])
+        state.games[chat_id] = GameState(day=0, is_day=False, users=[], votes=[], interaction_history=[])
     else:
         print(f"game in chat {chat_id} already exists")
