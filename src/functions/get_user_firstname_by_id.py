7b9dc616d8dee2592d4fc74e534e956d3d323eea
illia hrushchenko <gr.illya13@gmail.com>
2023-06-26 23:54:17 +0300

    Fixed get_user_firstname_by_id.py

--- src/functions/get_user_firstname_by_id.py
+++ src/functions/get_user_firstname_by_id.py
@@ -1,11 +1,9 @@
 from src.state import UserState
 from src.state import State
 
-class GetFirstName:
-    def get_user_firstname_by_id(chat_id: int, user_id: int):
-        state = State()
-        game = state.games[chat_id]
 
-        for user in game.users:
-            if user.user_id == user_id:
-                return user.first_name
+def get_user_firstname_by_id(chat_id: int, user_id: int):
+    state = State()
+    game = state.games[chat_id]
+
+    return list(filter(lambda x: x.user_id is user_id, game.users))[0]
