421b705c90b52ee74ff2afbbc18857b3268af82f
NoFC Pepukcoder <pepukcoder@gmail.com>
2023-06-28 22:49:00 +0600

    vote_for fixed

--- src/functions/vote_lynch.py
+++ src/functions/vote_lynch.py
@@ -1,18 +1,25 @@
 from aiogram import Dispatcher, types, Bot
 from src.state import State, ChatVoteState
 from src.functions import Delete
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 async def vote_lynch(chat_id, bot: Bot):
     state = State()
     game = state.games[chat_id]
     true_count, false_count = game.count_votes()
-    user = game.chat_votes.vote_for
-    msg_id = game.chat_votes.message_id
-    if true_count > false_count:
-        await bot.delete_message(chat_id, msg_id)
-        await bot.send_message(chat_id, f"*Заебашен {user}!*\n👍{true_count} | 👎🏿{false_count}", parse_mode='Markdown')
-        Delete.delete_all_elements_by_id(chat_id, [user])
-    else:
-        await bot.send_message(chat_id, f"Из-за того, что жители только и делали, что пиздели, они не смогли договориться, кого линчевать.", parse_mode='Markdown')
+    try:
+        user = game.chat_votes.vote_for
+        msg_id = game.chat_votes.message_id
+        if true_count > false_count:
+            await bot.delete_message(chat_id, msg_id)
+            await bot.send_message(chat_id, f"*Заебашен {user}!*\n👍{true_count} | 👎🏿{false_count}",
+                                   parse_mode='Markdown')
+            Delete.delete_all_elements_by_id(chat_id, [user])
+        else:
+            await bot.send_message(chat_id,
+                                   f"Из-за того, что жители только и делали, что пиздели, они не смогли договориться, кого линчевать.",
+                                   parse_mode='Markdown')
+    except TypeError:
+        await bot.send_message(chat_id,
+                               f"Из-за того, что жители только и делали, что пиздели, они не смогли договориться, кого линчевать.",
+                               parse_mode='Markdown')
--- src/state/vote_state.py
+++ src/state/vote_state.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import List, Tuple
 
+
 @dataclass
 class VoteState:
     vote_subject: int
