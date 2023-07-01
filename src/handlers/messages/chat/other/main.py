0b120f597ab8a5752ea76729e30492637a5134c4
NoFC Pepukcoder <pepukcoder@gmail.com>
2023-07-02 01:16:10 +0600

    removed regexp from other handlers

--- src/handlers/messages/chat/other/main.py
+++ src/handlers/messages/chat/other/main.py
@@ -7,12 +7,12 @@ bot = Bot(token=TgKeys.TOKEN, parse_mode='HTML')
 
 
 def register_other_handlers(dp: Dispatcher):
-    @dp.message_handler(Regexp(r'\bдон\b'))
-    async def don(message: types.Message):
-        await message.reply("гандон", parse_mode='Markdown')
 
-    @dp.message_handler(Regexp(r'(?i)\bтег\b'))
+    @dp.message_handler()
     async def tag(message: types.Message):
         words = message.text.split()
-        if len(words) == 1:
+        print(words)
+        if len(words) == 1 and words[0].lower() == 'тег':
             await message.reply(random.choice(["#непиздит", "#пиздит"]))
+        if 'дон' in words:
+            await message.reply('гандон')
